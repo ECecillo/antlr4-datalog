dff29b2e1bd49655e819779497607e46747d2892
Enzo CECILLON <enzocecillon@MacBook-Pro-de-Enzo.local>
2023-06-09 18:53:57 +0200

    style: remove print and comments

--- src/DatalogInterpretVisitor.py
+++ src/DatalogInterpretVisitor.py
@@ -3,6 +3,7 @@ from DatalogParser import DatalogParser
 from DatalogVisitor import DatalogVisitor
 from datalog_types import Fact, Predicate, AggregateFunction, Rule
 
+
 def create_dict(column_names: List[str], data: List):
     return dict(zip(column_names, data))
 
@@ -12,7 +13,7 @@ class MyDatalogVisitor(DatalogVisitor):
         # Initialize any necessary data structures or variables here.
         # store all variable ids and values.
         self.dataPerEdbDictionnary = dict()
-        self.database : List[Fact] = []
+        self.database: List[Fact] = []
         # self.rulesToEvaluate: List[Union[Predicate, 'AggregateFunction']] = []
         self.rulesToEvaluate: List[Rule] = []
         # edb are keys and value are array of column name.
@@ -29,7 +30,6 @@ class MyDatalogVisitor(DatalogVisitor):
         return ctx.getText()[1:-1]  # Remove the ""
 
     def visitTermVariable(self, ctx) -> str:
-        print(f'Variable that will be returned {ctx.getText()}')
         return ctx.getText()
 
     # Créer une liste avec les noms de colonnes.
@@ -54,19 +54,15 @@ class MyDatalogVisitor(DatalogVisitor):
         # initialise la liste des noms de colonne de l'edb avec ce que l'on a récupéré.
         self.edbColumnName[edbName] = columnNameList
         self.dataPerEdbDictionnary[edbName] = []
-        # TODO: remove print
-        print(edbName, self.edbColumnName[edbName])
 
     def visitTermBase(self, ctx) -> str or int or bool:
         return [self.visit(ctx.term())]
 
     def visitTermList(self, ctx):
         valueList = self.visit(ctx.terms_l())
-        print(f'Having the following term list : {valueList}')
         # Récupère ce qui devrait être l'id du tuple à ajouter pour le mettre au début.
         value = self.visit(ctx.term())
         valueList.insert(0, value)
-        print(f'Visiting the following term list : {valueList}')
         return valueList
 
     def visitClauseFact(self, ctx) -> None:
@@ -81,33 +77,28 @@ class MyDatalogVisitor(DatalogVisitor):
         # Visit the IDB and create the relation in our memory array.
         headResult = self.visit(ctx.head)
         bodyResult = self.visit(ctx.body())
-        print(f'Clause Head {headResult}')
-        print(f'Clause Body{bodyResult}')
         self.rulesToEvaluate.append(Rule(headResult, bodyResult))
 
     def visitBodyBase(self, ctx):
         return [self.visit(ctx.literal())]
-    
+
     def visitBodyList(self, ctx):
         predicate_list = self.visit(ctx.body())
         first_predicate = self.visit(ctx.literal())
         predicate_list.insert(0, first_predicate)
         return predicate_list
 
-    def visitPredicateDecl(self, ctx): 
-        # Creating a Predicate Object using class decl 
+    def visitPredicateDecl(self, ctx):
+        # Creating a Predicate Object using class decl
         predicate_name = self.visit(ctx.predicate_sym())
-        variables = self.visit(ctx.terms_l()) 
-        print(f'Predicate {Predicate(predicate_name, variables)}')
+        variables = self.visit(ctx.terms_l())
         return Predicate(predicate_name, variables)
 
     def visitAggregateDecl(self, ctx):
         operation = ctx.AGGREGATE().getText()
-        print(f'Aggregate Operation {operation}')
         variables = self.visit(ctx.terms_l())
         if len(variables) > 2:
-            return "Erreur: une aggregation prend 2 arguments (input_var, output_var)" 
+            return "Erreur: une aggregation prend 2 arguments (input_var, output_var)"
         else:
             input_var, output_var = variables
-            print(f'Input variable of aggregate is {input_var} and output {output_var}')
             return AggregateFunction(operation, input_var, output_var)
--- src/utils.py
+++ src/utils.py
@@ -7,9 +7,9 @@ def match_and_bind(predicate: Predicate, EDB: List[Fact]):
         if fact.predicate == predicate.name and len(fact.values) == len(predicate.terms):
             yield {term: value for term, value in zip(predicate.terms, fact.values)}
 
-def apply_aggregate(aggregate: AggregateFunction, 
+def apply_aggregate(aggregate: AggregateFunction,
                     variable_bindings: List[Dict[str, Union[str, int]]]
-    ) -> Union[str, int, float]:
+                    ) -> Union[str, int, float]:
     variable_to_aggr = aggregate.input_variable
     values_sum = 0
     values_count = 0
@@ -33,76 +33,63 @@ def apply_aggregate(aggregate: AggregateFunction,
     elif aggregate.function == 'AVG':
         return values_sum / values_count
 
+
 def construct_fact(head: Predicate,
                    variable_bindings: Dict[str, Union[str, int]]) -> Fact:
     fact_values = [variable_bindings[var] for var in head.terms]
     return Fact(head.name, fact_values)
 
+
 def datalog_engine_evaluation(datalog_program: List[Rule],
                               EDB: List[Fact]) -> List[Fact]:
     derived_facts = []
-    new_facts = True
 
-    while new_facts:
-        new_facts = False
+    for rule in datalog_program:
+        head, body = rule.head, rule.body
+        # Initialize a list of all variable bindings for this rule
+        all_variable_bindings = [{}]
 
-        for rule in datalog_program:
-            head, body = rule.head, rule.body
-            # Initialize a list of all variable bindings for this rule
-            all_variable_bindings = [{}]
+        for predicate in body:
+            if isinstance(predicate, Predicate):
+                # Get all matching bindings for this predicate
+                predicate_bindings = list(match_and_bind(predicate, EDB))
 
-            for predicate in body:
-                if isinstance(predicate, Predicate):
-                    # Get all matching bindings for this predicate
-                    predicate_bindings = list(match_and_bind(predicate, EDB))
-                    print(
-                        f'All matching binding at begining for {predicate}: {all_variable_bindings} and predicate_binding return is : {predicate_bindings}')
+                if all_variable_bindings.count({}) == 1 and len(predicate_bindings) > 1:
+                    all_variable_bindings = predicate_bindings
+                    continue
 
-                    if all_variable_bindings.count({}) == 1 and len(predicate_bindings) > 1:
-                        print("Is empty because starting evaluation")
-                        all_variable_bindings = predicate_bindings
-                        continue
-                   
-                    # index all bindings in a dictionnary.
-                    index = {}
-                    for bindings in all_variable_bindings:
-                        key = tuple(bindings[var] for var in predicate.terms if var in bindings)
-                        index.setdefault(key, []).append(bindings)
-                    test = [] 
-                    print(f'The index content for the current predicate {predicate} : {index}')
-          
-                    # Create a new list to hold the updated variable bindings
-                    new_variable_bindings = [] 
-                    # For each combination of existing and new bindings...
-                    for existing_bindings in all_variable_bindings:
-                        for new_bindings in predicate_bindings:
-                            common_keys = set(existing_bindings.keys()).intersection(new_bindings.keys())
-                            if all(existing_bindings[key] == new_bindings[key] for key in common_keys):
-                                merged_bindings = {**existing_bindings, **new_bindings}
-                                print(f'Merged bindings: {merged_bindings}')
-                                new_variable_bindings.append(merged_bindings)
-                            # for variable, value in new_bindings.items():
-                            #     if variable in existing_bindings and existing_bindings[variable] == value:
-                            #         new_binding = new_bindings.copy()
-                            #         print(
-                            #             f'Matching binding new variable {variable} with {new_binding} and old binding is {existing_bindings}')
-                            #         new_variable_bindings.append(new_binding)
-                    
-                    # Replace the list of all variable bindings with the updated list
-                    all_variable_bindings = new_variable_bindings
-                    print(
-                        f'All matching binding at end of loop for {predicate}: {all_variable_bindings}')
-                elif isinstance(predicate, AggregateFunction):
-                    result = apply_aggregate(predicate, all_variable_bindings)
-                    # Add the result to each individual binding
-                    for binding in all_variable_bindings:
-                        binding[predicate.output_variable] = result
+                # index all bindings in a dictionnary.
+                index = {}
+                for bindings in all_variable_bindings:
+                    key = tuple(bindings[var]
+                                for var in predicate.terms if var in bindings)
+                    index.setdefault(key, []).append(bindings)
 
-            # Construct derived facts for each set of variable bindings
-            for variable_bindings in all_variable_bindings:
-                derived_fact = construct_fact(head, variable_bindings)
+                # Create a new list to hold the updated variable bindings
+                new_variable_bindings = []
+                # For each combination of existing and new bindings...
+                for existing_bindings in all_variable_bindings:
+                    for new_bindings in predicate_bindings:
+                        common_keys = set(existing_bindings.keys()).intersection(
+                            new_bindings.keys())
+                        if all(existing_bindings[key] == new_bindings[key] for key in common_keys):
+                            merged_bindings = {
+                                **existing_bindings, **new_bindings}
+                            new_variable_bindings.append(merged_bindings)
 
-                if not any(fact.same_as(derived_fact) for fact in derived_facts):
-                    derived_facts.append(derived_fact)
+                # Replace the list of all variable bindings with the updated list
+                all_variable_bindings = new_variable_bindings
+            elif isinstance(predicate, AggregateFunction):
+                result = apply_aggregate(predicate, all_variable_bindings)
+                # Add the result to each individual binding
+                for binding in all_variable_bindings:
+                    binding[predicate.output_variable] = result
+
+        # Construct derived facts for each set of variable bindings
+        for variable_bindings in all_variable_bindings:
+            derived_fact = construct_fact(head, variable_bindings)
+
+            if not any(fact.same_as(derived_fact) for fact in derived_facts):
+                derived_facts.append(derived_fact)
 
     return derived_facts
